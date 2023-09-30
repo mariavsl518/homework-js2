801365ec5a679e904391b5752e0454b7e104dba2
mariavsl518 <mariavsl518@gmail.com>
2023-09-30 17:26:12 +0200

    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('afterbegin', markup(galleryItems))
+gallery.addEventListener('click', handlerClick())
+
+function markup(arr) { 
+    return arr.map(({ preview, original, description }) =>
+        `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="large-image.jpg"
+            alt="${description}"
+            />
+        </a>
+        </li>`).join('');
+}
+
+const instance = basicLightbox.create(`
+	 <img
+            class="gallery__image"
+            src="${original}"
+            data-source="large-image.jpg"
+            alt="${description}"
+            />
+`)
+
+function handlerClick(event) { 
+    event.preventDefault();
+    instance.show();
+     if (event.target === event.currentTarget) {
+    return;
+  }
+}
 
 console.log(galleryItems);
