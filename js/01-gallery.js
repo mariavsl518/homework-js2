4e0e5339f9695b8b3ae57db1a24066e6c015685a
mariavsl518 <mariavsl518@gmail.com>
2023-10-01 22:06:26 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
+const instance = basicLightbox.create(lightbox());
 
 gallery.insertAdjacentHTML('afterbegin', markup(galleryItems))
 gallery.addEventListener('click', handlerClick)
@@ -19,15 +20,24 @@ function markup(arr) {
         </a>
         </li>`).join('');
 }
+    
+function lightbox() { 
+    return `<img src="" width="800" height="600">`
+}
 
 function handlerClick(event) {
     event.preventDefault();
 
-    const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-    `)
-
     if (event.target === event.currentTarget) {
         return;
-    } else { return instance.show() }
+    } else {
+        // add url link here
+        return instance.show(()=> document.addEventListener('keydown', escapeClose))
+    }
 }
+
+function escapeClose(event) {
+        if (event.code === 'Escape') {
+            return instance.close()
+        }
+    }
